39c9ae807fe02a3954186268d6d70591299f7d74
Aman Singh <amansingh9097@gmail.com>
2019-06-21 14:26:04 +0530

    added sqlalchemy engine

--- misc/db_connection.py
+++ misc/db_connection.py
@@ -1,18 +1,27 @@
 import pyodbc
+from sqlalchemy import create_engine
+import urllib.parse
+from misc.service_logger import serviceLogger as logger
 
-# paste your connection string here
+# connection string goes here
 server = 'tcp:dai-dev-azure-sql1.database.windows.net,1433'
 database = 'OIMT_CompetitiveLandscape'
-username = 'xxxxxxx'
-password = 'xxxxxxx'
-
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Uid='+username+';Pwd='+password)
-cursor = cnxn.cursor()
+username = 'xxxx'
+password = 'xxxx'
 
 
-# Driver={ODBC Driver 13 for SQL Server};Server=tcp:dai-dev-azure-sql1.database.windows.net,1433;
-# Database=OIMT_CompetitiveLandscape;Uid=daiadmin@dai-dev-azure-sql1;Pwd={your_password_here};
-# Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+params = urllib.parse.quote_plus('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Uid='+username+';Pwd='+password)
+cnxn_string = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine = create_engine(cnxn_string)
+engine.connect()
+logger.info("Azure SQL Database Server Connection status: ... OK")
 
 
+
+# from sqlalchemy.sql import text
+# with engine.connect() as conn:
+    # query = text("""SELECT * FROM dbo.iati_txn""")
+    # res = conn.execute(query)
+    # for row in res:
+    #   print(row)
 
